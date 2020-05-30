eff550f929ae5a176e60fc647fd19ced4648fced
Samyak Shreyash <sam.shreyash@gmail.com>
2020-05-30 08:36:18 +0530

    Restarting Project
    To Evade NDK version Issue
    To properly manage versioning of each file

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,19 @@
-import 'package:dotodo/models/user.dart';
-import 'package:dotodo/screens/wrapper.dart';
-import 'package:dotodo/services/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData.dark(),
+    title: 'DoToDo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
     home: MyApp(),
   ));
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthService().user,
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        home: Wrapper(),
-      ),
-    );
+    return Scaffold();
   }
 }
